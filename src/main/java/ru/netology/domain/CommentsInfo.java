bafcaff1926d2a19887bd06fb2f75c6b6e724b4e
Marusya-Belova <m9175797050@gmail.com>
2022-01-11 21:12:00 +0300

    add files

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+
+public class CommentsInfo {
+    private int count;
+    private boolean canPost;
+
+    // + get/set на все поля
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+
+public class Post {
+    private int id;
+    private int ownerId;
+    private CommentsInfo commentsInfo;
+    private int viewsPost;
+    private int likesPost;
+    private int authorID;
+    private int reportsPost;
+    private String datePost;
+    private String textPost;
+
+
+
+    // + get/set на все поля
+}
