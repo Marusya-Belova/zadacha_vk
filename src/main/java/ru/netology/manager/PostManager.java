408f04a3288d96535ed5731dd68478ea2817b54f
Marusya-Belova <m9175797050@gmail.com>
2022-01-17 09:37:20 +0300

    reformat code

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,9 +5,9 @@ import ru.netology.domain.Post;
 public class PostManager {
     private Post[] posts;
 
-    public Post[] search() {
+    public Post[] search(int ownerId,String domain, String query, boolean ownersOnly, int count, int offset) {
         return null;
     }
-    public void delete() {
+    public void delete(int ownerId, int postId) {
     }
 };
